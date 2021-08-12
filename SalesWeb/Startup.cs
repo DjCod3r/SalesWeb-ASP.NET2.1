61e3ea1cc10f6780b1294bdc5748d2d710bdacb7
DjCod3r <Djonatanschvambach@gmail.com>
2021-08-12 20:40:23 -0300

    ScaffoldDepartment

--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
 
 namespace SalesWeb
 {
@@ -33,6 +35,9 @@ namespace SalesWeb
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,7 +61,7 @@ namespace SalesWeb
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Departments}/{action=Index}/{id?}");
             });
         }
     }
