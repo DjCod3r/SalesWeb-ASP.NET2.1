b3f2e87d4865c9ee26bf3a51a5db8e0daaabbb4f
DjCod3r <Djonatanschvambach@gmail.com>
2021-08-18 15:02:30 -0300

    Sellers

---
+++ SalesWeb/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWeb.Data;
+using SalesWeb.Models;
+using SalesWeb.Models.ViewModels;
+
+namespace SalesWeb.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebContext _context;
+
+        public SellerService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
